b149bf0863e2d3b5f4bb6599c621504a3ff0d635
Andrew Roberts <andrewzr@udel.edu>
2023-03-05 22:00:09 -0500

    Completed RevealAnswer, ChangeType and StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,31 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [quizInProgress, setQuizProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+    return (
+        <span>
+            <Button
+                onClick={() => {
+                    setQuizProgress(true);
+                    setAttempts(attempts - 1);
+                }}
+                disabled={quizInProgress || attempts < 1}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => setQuizProgress(false)}
+                disabled={!quizInProgress}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => setAttempts(attempts + 1)}
+                disabled={quizInProgress}
+            >
+                Mulligan
+            </Button>
+        </span>
+    );
 }
