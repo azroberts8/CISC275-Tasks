6245f8ddf6db1c82c7fae1789d18bfe6a75943ce
Andrew Roberts <andrewzr@udel.edu>
2023-03-05 22:00:09 -0500

    Completed RevealAnswer, ChangeType and StartAttempt

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,11 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setValue] = useState<boolean>(false);
+    return (
+        <span>
+            <Button onClick={() => setValue(!visible)}>Reveal Answer</Button>
+            <p>{visible ? "The answer is 42" : ""}</p>
+        </span>
+    );
 }
